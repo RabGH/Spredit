7539f58cf310b926d084610f77f15e65e6856f9d
RabGh <rabiighais@gmail.com>
2023-06-22 21:20:15 +0300

    fixing code duplication with FC sign in & up

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,11 +1,10 @@
-import { buttonVariants } from "@/components/ui/Button";
-import Link from "next/link";
-import { FC } from "react";
-import { cn } from "@/lib/utils";
 import SignUp from "@/components/SignUp";
+import { buttonVariants } from "@/components/ui/Button";
+import { cn } from "@/lib/utils";
 import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 
-const page: FC = () => {
+const page = () => {
   return (
     <div className="absolute inset-0">
       <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
