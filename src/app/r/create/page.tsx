d9bb951bfddea4362b257f32d47d134ac6a9bbfc
RabGh <rabiighais@gmail.com>
2023-06-22 23:01:10 +0300

    Add subreddits providers, input shadcn ui

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -1,9 +1,27 @@
 "use client";
 
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/Input";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { CreateSubredditPayload } from "@/lib/validators/subreddit";
 
 const Page = () => {
   const [input, setInput] = useState<string>("");
+  const router = useRouter();
+
+  //! check fullstack type safety
+  const { mutate: createCommunity, isLoading } = useMutation({
+    mutationFn: async () => {
+      const payload: CreateSubredditPayload = {
+        name: input,
+      };
+      const { data } = await axios.post("/api/subreddit", payload);
+      return data as string;
+    },
+  });
 
   return (
     <div className="container flex items-center h-full max-w-3xl mx-auto">
@@ -24,8 +42,25 @@ const Page = () => {
             <p className="absolute text-sm left-0 w-8 inset-y-0 grid place-items-center text-zinc-400">
               r/
             </p>
-            
+            <Input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              className="pl-6"
+            />
           </div>
+        </div>
+
+        <div className="flex justify-end gap-4">
+          <Button variant="subtle" onClick={() => router.back()}>
+            Cancel
+          </Button>
+          <Button
+            isLoading={isLoading}
+            disabled={input.length === 0}
+            onClick={() => createCommunity()}
+          >
+            Create Community
+          </Button>
         </div>
       </div>
     </div>
