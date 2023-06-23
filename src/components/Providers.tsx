c550c68ed1809c9db27b90d580e32cfe3cbb5b3b
RabGh <rabiighais@gmail.com>
2023-06-23 06:54:38 +0300

    declare query outside param & config.ts scroll add

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,10 +2,9 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
+const queryClient = new QueryClient();
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = new QueryClient();
-
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider>{children}</SessionProvider>
