4b7f1179679f588fcf46842dbd176089d8706201
RabGh <rabiighais@gmail.com>
2023-06-23 16:30:29 +0300

    comment/vote fix & can vote on comments & replies

--- src/components/PostComment.tsx
+++ src/components/PostComment.tsx
@@ -14,6 +14,7 @@ import { Textarea } from "@/components/ui/Textarea";
 import { useMutation } from "@tanstack/react-query";
 import { CommentRequest } from "@/lib/validators/comment";
 import axios from "axios";
+import { toast } from "@/hooks/use-toast";
 
 type ExtendedComment = Comment & {
   votes: CommentVote[];
@@ -51,6 +52,17 @@ const PostComment: FC<PostCommentProps> = ({
         payload
       );
       return data;
+    },
+    onError: (err) => {
+      return toast({
+        title: "Something went wrong",
+        description: "Comment was not posted, please try again.",
+        variant: "destructive",
+      });
+    },
+    onSuccess: () => {
+      router.refresh();
+      setIsReplying(false);
     },
   });
 
