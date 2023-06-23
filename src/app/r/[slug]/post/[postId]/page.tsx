0d6a646be2039bc16d6476d8b9b1969ff54bf6dd
RabGh <rabiighais@gmail.com>
2023-06-23 11:30:22 +0300

    stream important data first in subreddit page

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,8 +1,12 @@
+import { buttonVariants } from "@/components/ui/Button";
 import { db } from "@/lib/db";
 import { redis } from "@/lib/redis";
 import { CachedPost } from "@/types/redis";
 import { Vote, User, Post } from "@prisma/client";
 import { notFound } from "next/navigation";
+import { Suspense } from "react";
+import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
+import PostVoteServer from "@/components/post-vote/PostVoteServer";
 
 interface PageProps {
   params: {
@@ -37,10 +41,46 @@ const page = async ({ params }: PageProps) => {
   return (
     <div>
       <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
-        
+        <Suspense fallback={<PostVoteShell />}>
+          {/* @ts-expect-error server component */}
+          <PostVoteServer
+            postId={post?.id ?? cachedPost.id}
+            getData={async () => {
+              return await db.post.findUnique({
+                where: {
+                  id: params.postId,
+                },
+                include: {
+                  votes: true,
+                },
+              });
+            }}
+          />
+        </Suspense>
       </div>
     </div>
   );
 };
+
+function PostVoteShell() {
+  return (
+    <div className="flex items-center flex-col pr-6 w-20">
+      {/* upvote */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigUp className="h-5 w-5 text-zinc-700" />
+      </div>
+
+      {/* score */}
+      <div className="text-center py-2 font-medium text-sm text-zinc-900">
+        <Loader2 className="h-3 2-3 animate-spin" />
+      </div>
+
+      {/* downvote */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigDown className="h-5 w-5 text-zinc-700" />
+      </div>
+    </div>
+  );
+}
 
 export default page;
