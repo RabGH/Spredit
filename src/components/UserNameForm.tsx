7afd3d93e51b45260e290e4160f13df761a78fac
RabGh <rabiighais@gmail.com>
2023-06-23 17:55:38 +0300

    rename username using react hook form & card shad

--- src/components/UserNameForm.tsx
+++ src/components/UserNameForm.tsx
@@ -5,6 +5,21 @@ import { FC } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { User } from "@prisma/client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/Card";
+import { Label } from "@/components/ui/Label";
+import { Input } from "@/components/ui/Input";
+import { Button } from "@/components/ui/Button";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { toast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
 interface UserNameFormProps {
   user: Pick<User, "id" | "username">;
@@ -22,7 +37,76 @@ const UserNameForm: FC<UserNameFormProps> = ({ user }) => {
     },
   });
 
-  return <form onSubmit={handleSubmit(() => {})}></form>;
+  const router = useRouter();
+
+  const {} = useMutation({
+    mutationFn: async ({ name }: UsernameRequest) => {
+      const payload: UsernameRequest = { name };
+
+      const { data } = await axios.patch(`/api/username`, payload);
+      return data;
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 409) {
+          return toast({
+            title: "Username already taken.",
+            description:
+              "Please choose a unique user name that doesn't already exist.",
+            variant: "destructive",
+          });
+        }
+      }
+
+      toast({
+        title: "There was an error.",
+        description: "Could not rename user.",
+        variant: "destructive",
+      });
+    },
+    onSuccess: () => {
+      toast({
+        description: "Username updated!",
+      });
+      router.refresh();
+    },
+  });
+
+  return (
+    <form onSubmit={handleSubmit(() => {})}>
+      <Card>
+        <CardHeader>
+          <CardTitle>You username</CardTitle>
+          <CardDescription>
+            Enter a display name that will be shown on your profile.
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <div className="relative grid gap-1">
+            <div className="absolute top-0 left-0 w-8 h-10 grid place-items-center">
+              <span className="text-sm text-zinc-400"></span>
+            </div>
+            <Label className="sr-only" htmlFor="name">
+              Name
+            </Label>
+            <Input
+              id="name"
+              className="w-[400px] pl-6"
+              size={32}
+              {...register("name")}
+            />
+            {errors?.name && (
+              <p className="px-1 text-xs text-red-600">{errors.name.message}</p>
+            )}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button>Change name</Button>
+        </CardFooter>
+      </Card>
+    </form>
+  );
 };
 
 export default UserNameForm;
