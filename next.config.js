5a912253f09c466f4034bd23eb695a48d304434c
RabGh <rabiighais@gmail.com>
2023-06-23 19:45:09 +0300

    Debugging testing path require next

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
-/** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   images: {
     domains: ["uploadthing.com", "lh3.googleusercontent.com"],
   },
   experimental: {
     appDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.join(__dirname, "src");
+    return config;
   },
 };
 
