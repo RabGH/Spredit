6e641c8a0987d96dd45f0e8e9dee8a1fb6ee45b4
RabGh <rabiighais@gmail.com>
2023-06-23 12:34:25 +0300

    Add label & create comment section pages & votes

--- src/components/CommentsSection.tsx
+++ src/components/CommentsSection.tsx
@@ -1,8 +1,7 @@
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { Comment, CommentVote, User } from "@prisma/client";
-import CreateComment from "./CreateComment";
-import PostComment from "@/components/PostComment";
+import PostComment from "./PostComment";
 
 type ExtendedComment = Comment & {
   votes: CommentVote[];
@@ -44,8 +43,6 @@ const CommentsSection = async ({ postId }: CommentsSectionProps) => {
     <div className="flex flex-col gap-y-4 mt-4">
       <hr className="w-full h-px my-6" />
 
-      <CreateComment postId={postId} />
-
       <div className="flex flex-col gap-y-6 mt-4">
         {comments
           .filter((comment) => !comment.replyToId)
@@ -66,42 +63,8 @@ const CommentsSection = async ({ postId }: CommentsSectionProps) => {
             return (
               <div key={topLevelComment.id} className="flex flex-col">
                 <div className="mb-2">
-                  <PostComment
-                    comment={topLevelComment}
-                    currentVote={topLevelCommentVote}
-                    votesAmt={topLevelCommentVotesAmt}
-                    postId={postId}
-                  />
+                  <PostComment comment={topLevelComment} />
                 </div>
-
-                {/* Render replies */}
-                {topLevelComment.replies
-                  .sort((a, b) => b.votes.length - a.votes.length) // Sort replies by most liked
-                  .map((reply) => {
-                    const replyVotesAmt = reply.votes.reduce((acc, vote) => {
-                      if (vote.type === "UP") return acc + 1;
-                      if (vote.type === "DOWN") return acc - 1;
-                      return acc;
-                    }, 0);
-
-                    const replyVote = reply.votes.find(
-                      (vote) => vote.userId === session?.user.id
-                    );
-
-                    return (
-                      <div
-                        key={reply.id}
-                        className="ml-2 py-2 pl-4 border-l-2 border-zinc-200"
-                      >
-                        <PostComment
-                          comment={reply}
-                          currentVote={replyVote}
-                          votesAmt={replyVotesAmt}
-                          postId={postId}
-                        />
-                      </div>
-                    );
-                  })}
               </div>
             );
           })}
---
+++ src/components/CreateComment.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+interface CreateCommentProps {}
+
+const CreateComment: FC<CreateCommentProps> = ({}) => {
+  return <div className="grid w-full gap-1.5">
+    <Label />
+  </div>;
+};
+
+export default CreateComment;
