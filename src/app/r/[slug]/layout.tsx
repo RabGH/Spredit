5ecd4008f30a24010ada2e3e739467055520205c
RabGh <rabiighais@gmail.com>
2023-06-23 19:17:31 +0300

    Add to feed button

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -6,6 +6,7 @@ import { format } from "date-fns";
 import { Metadata } from "next";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import ToFeedButton from "@/components/ToFeedButton";
 
 export const metadata: Metadata = {
   title: "Spredit",
@@ -60,7 +61,7 @@ const Layout = async ({
   return (
     <div className="sm:container max-w-7xl mx-auto h-full pt-12">
       <div>
-        {/* TODO: Button to take us back */}
+        <ToFeedButton />
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
           <ul className="flex flex-col col-span-2 space-y-6">{children}</ul>
