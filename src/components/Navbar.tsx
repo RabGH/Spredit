9728549f47e9d6383a0ac52f9079f35dbc349355
RabGh <rabiighais@gmail.com>
2023-08-26 04:16:51 +0300

    Testing navbar responsiveness phones

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = async () => {
 
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-[1.5rem] sm:py-[1rem]">
-      <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
+      <div className="container p-2 max-w-7xl h-full flex items-center justify-between gap-2">
         {/* logo */}
         <Link href="/" className="flex gap-2 items-center">
           <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
